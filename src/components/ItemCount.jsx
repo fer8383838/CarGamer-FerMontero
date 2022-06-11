61c614d37af75ba144a24f0c1f225fe5a36941c4
desarrolladorweb123 <lmonterodiaz01@gmail.com>
2022-06-11 11:52:53 -0500

    11Junio

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -8,8 +8,12 @@ export default function ItemCount({ stock, initial, onAdd }) {
         <br />
             <h3>Stock {stock}</h3>
             <h3>Cantidad {x}</h3>
-            <button onClick={()=> { x < 5 ? setX(x+1) : alert('no hhay stock')}}>+</button>
-            <button onClick={()=> { x > 1 ? setX(x-1) : alert('no fastidies')}}>-</button>
+            <button onClick={()=> {setX(x+1)}}>+</button>
+            <button onClick={()=> { x > 1 ? setX(x-1) : alert('no fastidies')}}>-</button><br />
+            <button type="button" class="btn btn-primary btn-lg"
+            onClick={()=> { x < 5 ? alert(`Se han agregado ${x} productos`) : alert('no hay stock')}}>
+                Agregar Carrito
+            </button>
         </>
     )
 }
