8e3f2c1e9a3752dc31531c0db0f72a2325737e44
desarrolladorweb123 <lmonterodiaz01@gmail.com>
2022-06-13 10:35:32 -0500

    13Junio

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,53 +1,14 @@
-import { useEffect, useState } from "react";
-import ItemList from "./ItemList";
 import Item from "./Item";
-
-const MOCK_ITEMS = [
-    {
-        id: 1,
-        title: "hola",
-        price: 10,
-        img: "https://via.placeholder.com/64",
-    },
-    {
-        id: 2,
-        tittle: "hola",
-        price: 20,
-        img: "https://via.placeholder.com/64",
-    },
-    {
-        id: 3,
-        tittle: "hola",
-        price: 30,
-        img: "https://via.placeholder.com/64",
-    },
-];
-
-export default function ItemList({ items }) {
-
-    useEffect(() => {
-        const getItems = () => {
-            return new Promise((res, rej) => {
-                setTimeout(() => {
-                    res(MOCK_ITEMS);
-                }, 2000);
-            });
-        };
-
-        getItems()
-            .then((res) => {
-                setItems(res);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    });
-
+export default function ItemList( {items} ) {
     console.log(items)
     return (
         <>
-            <h1>ItemListContainer</h1>
-            <ItemList items={items} />
+            <h2>ItemList</h2>
+            <div>{items.map((e, index) => (
+            <>
+            <Item id={e.id} title={e.title} price={e.price} index={index+1} img={e.img}/>
+            </>
+            ))}</div>
         </>
     )
 }
