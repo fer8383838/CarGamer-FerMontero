a50556d55a8a76adb8ccdf0d8ea075430a6a3440
desarrolladorweb123 <lmonterodiaz01@gmail.com>
2022-06-13 10:46:48 -0500

    13Junio

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,48 +1,23 @@
-import { useEffect, useState } from "react";
 import ItemList from "./ItemList";
-
-const MOCK_ITEMS = [
-  {
+export default function ItemListContainer() {
+  const items = [{
     id: 1,
     title: "hola",
     price: 10,
-    img: "https://via.placeholder.com/64",
+    img: "https://via.placeholder.com/64"
   },
   {
     id: 2,
     tittle: "hola",
     price: 20,
-    img: "https://via.placeholder.com/64",
+    img: "https://via.placeholder.com/64"
   },
   {
     id: 3,
     tittle: "hola",
     price: 30,
-    img: "https://via.placeholder.com/64",
-  },
-];
-
-export default function ItemListContainer() {
-
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    const getItems = () => {
-      return new Promise((res, rej) => {
-        setTimeout(() => {
-          res(MOCK_ITEMS);
-        }, 2000);
-      });
-    };
-
-    getItems()
-      .then((res) => {
-        setItems(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  });
+    img: "https://via.placeholder.com/64"
+  }]
 
   return (
     <>
